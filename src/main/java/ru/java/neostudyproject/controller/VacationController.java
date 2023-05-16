9e368abd892a21127e91f57f18ed4fe402cbfa91
khitao <108884890+khitao@users.noreply.github.com>
2023-05-16 23:02:51 +0300

    some new

--- src/main/java/ru/java/neostudyproject/controller/VacationController.java
+++ src/main/java/ru/java/neostudyproject/controller/VacationController.java
@@ -1,2 +1,25 @@
-package ru.java.neostudyproject.controller;public class VacationController {
+package ru.java.neostudyproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.java.neostudyproject.service.VacationCalculator;
+
+
+@RestController
+@RequiredArgsConstructor
+public class VacationController {
+
+    private final VacationCalculator vacationCalculator;
+
+    @GetMapping("/calculate")
+    public ResponseEntity<Double> calculateVacationAllowance(@RequestParam double averageSalary, @RequestParam int vacationDays, @RequestParam String vacationDates) {
+
+        double vacationAllowance = vacationCalculator.calculateVacationAllowance(averageSalary, vacationDays, vacationDates);
+
+        return ResponseEntity.ok(vacationAllowance);
+    }
+
 }
--- src/main/java/ru/java/neostudyproject/service/VacationCalculator.java
+++ src/main/java/ru/java/neostudyproject/service/VacationCalculator.java
@@ -1,2 +1,75 @@
-package ru.java.neostudyproject.service;public class VacationCalculator {
+package ru.java.neostudyproject.service;
+
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class VacationCalculator {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final List<LocalDate> holidays = new ArrayList<>();
+
+
+    static {
+        addHoliday("2023-01-01");
+        addHoliday("2023-01-02");
+        addHoliday("2023-01-03");
+        addHoliday("2023-01-04");
+        addHoliday("2023-01-05");
+        addHoliday("2023-01-06");
+        addHoliday("2023-02-23");
+        addHoliday("2023-02-24");
+        addHoliday("2023-03-08");
+        addHoliday("2023-05-01");
+        addHoliday("2023-05-08");
+        addHoliday("2023-05-09");
+        addHoliday("2023-06-12");
+        addHoliday("2023-11-06");
+    }
+
+    public static void addHoliday(String holidayDate) {
+        LocalDate holiday = LocalDate.parse(holidayDate, formatter);
+        holidays.add(holiday);
+    }
+
+    public double calculateVacationAllowance(double averageSalary, int vacationDays, String departureDate) {
+        double vacationAllowance = 0;
+
+        if (departureDate != null) {
+            LocalDate departure = LocalDate.parse(departureDate, formatter);
+            int workingDays = calculateWorkingDays(departure, vacationDays);
+            vacationAllowance = averageSalary * workingDays;
+        }
+
+        return vacationAllowance;
+    }
+
+    private int calculateWorkingDays(LocalDate departure, int vacationDays) {
+        int workingDays = 0;
+
+        for (int i = 0; i < vacationDays; i++) {
+            LocalDate date = departure.plusDays(i);
+            if (isWorkingDay(date)) {
+                workingDays++;
+            }
+        }
+
+        return workingDays;
+    }
+
+    private boolean isWorkingDay(LocalDate date) {
+        return !isWeekend(date) && !isHoliday(date);
+    }
+
+    private boolean isWeekend(LocalDate date) {
+        return date.getDayOfWeek().getValue() >= 6;
+    }
+
+    private boolean isHoliday(LocalDate date) {
+        return holidays.contains(date);
+    }
 }
